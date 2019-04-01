6be040cd15671ad1fd8dcaa3280baf7fd5a3daed
Adam Rozenek <adam.rozenek@gmail.com>
2019-04-01 22:34:16 +0200

    First implementation of user interface

---
+++ Catch X/src/Frame.java
@@ -0,0 +1,38 @@
+import javax.swing.JFrame;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import java.awt.Dimension;
+
+
+public class Frame extends JFrame {
+
+	private JButton X_agent;
+	private JPanel panel;
+
+	
+	
+	public Frame() {
+		super("Catch X");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
+		setMinimumSize(new Dimension (500, 500));
+		
+		panel = new JPanel();
+		Dimension panel_size = panel.getSize();
+		panel.setPreferredSize(new Dimension(40,40));
+		System.out.println(panel_size.height);
+		System.out.println(panel_size.width);
+		
+		
+		X_agent = new JButton("X");
+		X_agent.setVisible(true);
+		X_agent.setPreferredSize(new Dimension(45,45));
+		
+		panel.add(X_agent);
+		add(panel);
+	}
+	
+	public void print_size() {
+		System.out.println(panel.getSize());
+	}
+}
