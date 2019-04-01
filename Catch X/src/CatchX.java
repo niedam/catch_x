084409c1fa609dbea3f6ac7a46217942f6f87d0c
Adam Rozenek <adam.rozenek@gmail.com>
2019-04-01 21:56:16 +0200

    Prepared user interface of game

---
+++ Catch X/src/CatchX.java
@@ -0,0 +1,9 @@
+import java.awt.EventQueue;
+
+public class CatchX {
+
+	public static void main(String[] args) {
+		System.out.println("Catch X if you can! It' s not so hard");
+		Frame a = new Frame();
+	}
+}
