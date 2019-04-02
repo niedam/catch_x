21d7f3b4ee5cd24a5f6adbe594f4c61b06f6f456
Adam Rozenek <adam.rozenek@gmail.com>
2019-04-02 21:04:08 +0200

    Set absolute position of X button.

--- Catch X/src/Frame.java
+++ Catch X/src/Frame.java
@@ -1,39 +1,37 @@
 import javax.swing.JFrame;
 import javax.swing.JButton;
 import javax.swing.JPanel;
+
+import java.awt.Color;
+import java.awt.Container;
 import java.awt.Dimension;
+import java.awt.Insets;
 
 
 public class Frame extends JFrame {
 
-	
 	private JButton X_agent;
 	private JPanel panel;
-
-	
 	
 	public Frame() {
 		super("Catch X");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
 		setVisible(true);
 		setMinimumSize(new Dimension (500, 500));
 		
-		panel = new JPanel();
-		Dimension panel_size = panel.getSize();
-		panel.setPreferredSize(new Dimension(40,40));
-		System.out.println(panel_size.height);
-		System.out.println(panel_size.width);
-		
+		Container pane = getContentPane();
+		pane.setLayout(null);
 		
 		X_agent = new JButton("X");
-		X_agent.setVisible(true);
-		X_agent.setPreferredSize(new Dimension(45,45));
 		
-		panel.add(X_agent);
-		add(panel);
-	}
-	
-	public void print_size() {
-		System.out.println(panel.getSize());
+		pane.add(X_agent);
+		//pane.setBackground(Color.BLACK);
+		
+		
+		Insets insets = pane.getInsets();
+		
+		X_agent.setBounds(10 + insets.left, 20 + insets.top, 45, 45);
+		
 	}
 }
